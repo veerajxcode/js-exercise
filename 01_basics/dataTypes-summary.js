b3fbaff49a5806a94aac636295b768aad437af11
Veeraj Yadav <55523605+veerajxcode@users.noreply.github.com>
2024-05-08 08:08:12 +0000

    stack and heap done

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -13,5 +13,26 @@ console.log( id === anotherId)
 // Array, Objects, Functions
 
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
 
 
+// Stack gets the copy of the original value. So original value will remain same .
+let nameOne = "Veeraj"
+let nameTwo = nameOne
+nameTwo = "Yadav"
+console.log(nameOne)
+console.log(nameTwo)
+
+//Heap gets the reference from the original value. Original value can get change.
+let userOne = {
+    email: "xyz@g.com",
+    upi: "ade@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "abc@g.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)

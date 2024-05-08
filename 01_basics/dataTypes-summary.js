20c36367552024ab1f26d1fdcfb1fa3bc1d6d867
Veeraj Yadav <55523605+veerajxcode@users.noreply.github.com>
2024-05-08 05:35:10 +0000

    js datatype revision done

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,17 @@
+//Data Types divided into two parts
+
+// (Value) Primitive data type
+// 7 types
+// String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log( id === anotherId)
+
+// (Reference) Non primitive
+// Array, Objects, Functions
+
+
+
+

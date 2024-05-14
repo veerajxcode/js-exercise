efcc6e4a60d980ffbcb5c7f0a95d8161e1404021
Veeraj Yadav <55523605+veerajxcode@users.noreply.github.com>
2024-05-14 10:42:13 +0000

    function done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+function sayMyName(){
+console.log("V");
+console.log("E");
+console.log("E");
+console.log("R");
+console.log("A");
+console.log("J");
+}
+
+sayMyName();
+
+function addTwoNumbers(number1 , number2){
+    return number1 + number2
+}
+
+console.log(addTwoNumbers(5, 6));

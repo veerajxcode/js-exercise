49b44baf3fbac8c3d5fbf4243ec80fecfc15fa0f
Veeraj Yadav <55523605+veerajxcode@users.noreply.github.com>
2024-05-17 09:47:08 +0000

    loop concepts

---
+++ 05_iterations/forloop.js
@@ -0,0 +1,9 @@
+//for
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    if(element == 5){
+        console.log("5 is best!!");
+        continue
+    }
+    console.log(element);   
+}

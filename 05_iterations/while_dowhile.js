723dcb91cc688704bd9eb341ea794311a8a526ff
Veeraj Yadav <55523605+veerajxcode@users.noreply.github.com>
2024-05-22 04:48:06 +0000

    while_dowhile basics done

---
+++ 05_iterations/while_dowhile.js
@@ -0,0 +1,19 @@
+let index = 0
+
+while (index <= 20){
+    console.log(`Value for index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['flash', 'batman', 'superman']
+let arr = 0
+while(arr < myArray.length){
+    console.log(`Superhero name is ${myArray[arr]}`);
+    arr++;
+}
+
+let score = 1
+do {
+console.log(`Score is ${score}`);
+score++;
+}while(score <= 10);

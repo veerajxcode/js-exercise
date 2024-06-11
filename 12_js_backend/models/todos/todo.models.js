08289a4973e2c93ba37a3f9f00a763c273085ce6
Veeraj Yadav <55523605+veerajxcode@users.noreply.github.com>
2024-06-11 07:20:50 +0000

    creating models using mongoose

---
+++ 12_js_backend/models/todos/todo.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const todoSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    },
+    subTodos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "SubTodo"
+        }
+    ], //Array of Sub-Todos
+},{timestamps: true})
+
+export const todo = mongoose.model("todo", todoSchema)
+

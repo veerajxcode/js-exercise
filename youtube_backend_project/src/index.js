e510ec54c513a22a1d53bb4d412bd0baf22c4b1b
Veeraj Yadav <55523605+veerajxcode@users.noreply.github.com>
2024-06-13 06:49:32 +0000

    connect to mongo atlas

---
+++ youtube_backend_project/src/index.js
@@ -0,0 +1,52 @@
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv";
+//import mongoose from "mongoose";
+//import {DB_NAME} from "./constants";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+/*import express from "express";
+const app = express()*/
+
+/**
+ * Professional approach
+ * IIFE Approach
+ * An IIFE (Immediately Invoked Function Expression) is a Javascript
+ *  function that runs as soon as function is defined.
+ * Example:
+ * (function () {
+    // …
+    })();
+------------------------------------
+    (() => {
+    // …
+    })();
+------------------------------------
+    (async () => {
+    // …
+    })();
+
+    Here we are using async () approach:
+    An async () IFFE allows you to use await and for-await even in old browsers and
+    javascript runtimes that has no top-level await
+ */
+/*( async () =>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errr", (error) =>{
+            console.log("ERRR: ", error);
+            throw error
+        })
+        app.listen(process.env.port, () =>{
+            console.log(`It is running on ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()*/

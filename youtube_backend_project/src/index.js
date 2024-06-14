8e94f34bb005064e80d44fdd2a7941337f09198f
Veeraj Yadav <55523605+veerajxcode@users.noreply.github.com>
2024-06-14 06:24:47 +0000

    utils code discussion

--- youtube_backend_project/src/index.js
+++ youtube_backend_project/src/index.js
@@ -3,12 +3,21 @@ import dotenv from "dotenv";
 //import mongoose from "mongoose";
 //import {DB_NAME} from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server id running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+console.log(`Mongo db connection failed!! : ${err}`);
+})
 
 /*import express from "express";
 const app = express()*/
---
+++ youtube_backend_project/src/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+const asyncHandler = () => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+        catch((err) => next(err))
+    }
+}
+
+
+
+export {asyncHandler}
